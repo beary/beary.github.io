ea89764d35fe44fa6370bd5de04603d4f99e0b1e
beary <bearyme@outlook.com>
2021-06-22 00:25:56 +0800

    initial commit
    Signed-off-by: beary <bearyme@outlook.com>

---
+++ posts/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: 'Beary 的笔记本',
+  themeConfig: {
+    sidebar: [
+      {
+        isGroup: true,
+        text: 'Vue',
+        children: ['/vue/vue3-reactivity-system.md']
+      }
+    ]
+  }
+})
---
+++ posts/vue/vue3-reactivity-system.md
@@ -0,0 +1,8 @@
+---
+title: Vue 3 响应式原理
+description: 页面的描述
+---
+
+# Vue 3 响应式原理
+
+Hello
